a2564aaab2bb743ba5955ff077ac207369b28568
IthacaCow <cba840910@gmail.com>
2015-08-23 22:24:26 +0800

    make the welcome overlay a little bit responsive

--- js/general.js
+++ js/general.js
@@ -2,7 +2,16 @@ var contactFormURL = "https://docs.google.com/forms/d/1M5hfCZmdP1zNFttbXoJfeGJ0g
 
 function hideWelcomeOverlay () {
 	var d = document.getElementById("welcome");
+
+	// apply hide animation
 	d.className = d.className + " inactive";
+
+	// hide the element when the animation is finished.
+	setTimeout( 
+		function(){ 
+			d.className = d.className + " hide" 
+		}
+	, 500);
 }
 
 function redirectToContactForm () {
