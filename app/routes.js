aeae304f4b208a27491e11018983f023c8bafea2
IthacaCow <cba840910@gmail.com>
2015-08-26 10:44:21 +0800

    add backend && change directory structure && test Facebook API functionality

---
+++ app/routes.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var publicFileRoot = path.join(__dirname, './public');
+
+module.exports = function(app) {
+
+	//
+	// view -------------------------------------------------------------
+	//
+	app.get('/', function(req, res) {
+		res.sendFile('index.html', { root: publicFileRoot });
+	});
+
+	app.get('/test', function(req, res) {
+		res.sendFile('test.html', { root: publicFileRoot });
+	});
+
+
+	//
+	// api ---------------------------------------------------------------------
+	// 
+	app.get('/api/todos', function(req, res) {
+
+	});
+
+	app.post('/api/todos', function(req, res) {
+
+	});
+
+
+	
+};
